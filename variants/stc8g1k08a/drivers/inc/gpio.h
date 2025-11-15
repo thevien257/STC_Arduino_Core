bb74190d8427c1060edb6cd6c236a5f3c0c404d1
thevien257 <thevien2507@gmail.com>
2025-11-15 14:17:31 +0700

    feat: update pins mapping number

--- variants/stc8g1k08a/drivers/inc/gpio.h
+++ variants/stc8g1k08a/drivers/inc/gpio.h
@@ -5,45 +5,43 @@
 #include "Arduino.h"  // Include for bit manipulation macros
 
 // Pin mapping helpers
-#define GET_PORT(pin)  ((pin) >> 3)
-#define GET_BIT(pin)   ((pin) & 0x07)
-#define GET_MASK(pin)  (1 << GET_BIT(pin))
+#define GET_PORT(pin)  (((pin) < 4) ? PORT3 : PORT5)  // Pins 0-3 → port 3, 4+ → port 5
+#define GET_MASK(pin)  (1 << (pin))  // Direct mask from pin number
 
 // pinMode macro
 #define pinMode(pin, mode) do { \
     uint8_t _port = GET_PORT(pin); \
-    uint8_t _bit = GET_BIT(pin); \
     uint8_t _mask = GET_MASK(pin); \
     SET_BIT(P_SW2, 7); \
-    if (_port == 2) { \
+    if (_port == PORT3) { \
         SET_BIT_MASK(P3IE, _mask); \
         if ((mode) == OUTPUT) { \
-            CLEAR_BIT(P3M1, _bit); \
-            SET_BIT(P3M0, _bit); \
-            CLEAR_BIT(P3PU, _bit); \
+            CLEAR_BIT(P3M1, (pin)); \
+            SET_BIT(P3M0, (pin)); \
+            CLEAR_BIT(P3PU, (pin)); \
         } else if ((mode) == INPUT_PULLUP) { \
-            SET_BIT(P3M1, _bit); \
-            CLEAR_BIT(P3M0, _bit); \
-            SET_BIT(P3PU, _bit); \
+            SET_BIT(P3M1, (pin)); \
+            CLEAR_BIT(P3M0, (pin)); \
+            SET_BIT(P3PU, (pin)); \
         } else { \
-            SET_BIT(P3M1, _bit); \
-            CLEAR_BIT(P3M0, _bit); \
-            CLEAR_BIT(P3PU, _bit); \
+            SET_BIT(P3M1, (pin)); \
+            CLEAR_BIT(P3M0, (pin)); \
+            CLEAR_BIT(P3PU, (pin)); \
         } \
-    } else if (_port == 3) { \
+    } else if (_port == PORT5) { \
         SET_BIT_MASK(P5IE, _mask); \
         if ((mode) == OUTPUT) { \
-            CLEAR_BIT(P5M1, _bit); \
-            SET_BIT(P5M0, _bit); \
-            CLEAR_BIT(P5PU, _bit); \
+            CLEAR_BIT(P5M1, (pin)); \
+            SET_BIT(P5M0, (pin)); \
+            CLEAR_BIT(P5PU, (pin)); \
         } else if ((mode) == INPUT_PULLUP) { \
-            SET_BIT(P5M1, _bit); \
-            CLEAR_BIT(P5M0, _bit); \
-            SET_BIT(P5PU, _bit); \
+            SET_BIT(P5M1, (pin)); \
+            CLEAR_BIT(P5M0, (pin)); \
+            SET_BIT(P5PU, (pin)); \
         } else { \
-            SET_BIT(P5M1, _bit); \
-            CLEAR_BIT(P5M0, _bit); \
-            CLEAR_BIT(P5PU, _bit); \
+            SET_BIT(P5M1, (pin)); \
+            CLEAR_BIT(P5M0, (pin)); \
+            CLEAR_BIT(P5PU, (pin)); \
         } \
     } \
     CLEAR_BIT(P_SW2, 7); \
@@ -52,27 +50,25 @@
 // digitalWrite macro
 #define digitalWrite(pin, value) do { \
     uint8_t _port = GET_PORT(pin); \
-    uint8_t _bit = GET_BIT(pin); \
-    if (_port == 2) { \
-        WRITE_BIT(P3, _bit, value); \
-    } else if (_port == 3) { \
-        WRITE_BIT(P5, _bit, value); \
+    if (_port == PORT3) { \
+        WRITE_BIT(P3, (pin), value); \
+    } else if (_port == PORT5) { \
+        WRITE_BIT(P5, (pin), value); \
     } \
 } while(0)
 
 // digitalRead macro
 #define digitalRead(pin) \
-    ((GET_PORT(pin) == 2) ? (IS_BIT_SET(P3, GET_BIT(pin)) ? HIGH : LOW) : \
-     (GET_PORT(pin) == 3) ? (IS_BIT_SET(P5, GET_BIT(pin)) ? HIGH : LOW) : LOW)
+    ((GET_PORT(pin) == PORT3) ? (IS_BIT_SET(P3, (pin)) ? HIGH : LOW) : \
+     (GET_PORT(pin) == PORT5) ? (IS_BIT_SET(P5, (pin)) ? HIGH : LOW) : LOW)
 
 // digitalToggle macro
 #define digitalToggle(pin) do { \
     uint8_t _port = GET_PORT(pin); \
-    uint8_t _bit = GET_BIT(pin); \
-    if (_port == 2) { \
-        TOGGLE_BIT(P3, _bit); \
-    } else if (_port == 3) { \
-        TOGGLE_BIT(P5, _bit); \
+    if (_port == PORT3) { \
+        TOGGLE_BIT(P3, (pin)); \
+    } else if (_port == PORT5) { \
+        TOGGLE_BIT(P5, (pin)); \
     } \
 } while(0)
 
--- variants/stc8g1k08a/variant.h
+++ variants/stc8g1k08a/variant.h
@@ -6,78 +6,91 @@
 #include "drivers/inc/timer.h"
 
 // STC8G1K08A Register Definitions
-__sfr __at (0x80) P0;
-__sfr __at (0x81) SP;
-__sfr __at (0x82) DPL;
-__sfr __at (0x83) DPH;
-__sfr __at (0x87) PCON;
-__sfr __at (0x88) TCON;
-__sfr __at (0x89) TMOD;
-__sfr __at (0x8A) TL0;
-__sfr __at (0x8B) TL1;
-__sfr __at (0x8C) TH0;
-__sfr __at (0x8D) TH1;
-__sfr __at (0x8E) AUXR;
-__sfr __at (0x90) P1;
-__sfr __at (0x91) P1M1;
-__sfr __at (0x92) P1M0;
-__sfr __at (0x93) P0M1;
-__sfr __at (0x94) P0M0;
-__sfr __at (0x98) SCON;      // UART1 control register
-__sfr __at (0x99) SBUF;      // UART1 data buffer register
-__sfr __at (0x9D) IRCBAND;   // IRC band selection register
-__sfr __at (0x9E) LIRTRIM;   // IRC frequency trim register
-__sfr __at (0x9F) IRTRIM;    // IRC frequency adjustment register
-__sfr __at (0xA0) P2;
-__sfr __at (0xA2) P_SW1;     // Peripheral port switch register 1
-__sfr __at (0xA8) IE;
-__sfr __at (0xA9) SADDR;     // UART1 slave address register
-__sfr __at (0xB0) P3;
-__sfr __at (0xB1) P3M1;
-__sfr __at (0xB2) P3M0;
-__sfr __at (0xB8) IP;
-__sfr __at (0xB9) SADEN;     // UART1 slave address enable register
-__sfr __at (0xBA) P_SW2;     // Added for EAXFR access
-__sfr __at (0xC8) P5;
-__sfr __at (0xC9) P5M1;
-__sfr __at (0xCA) P5M0;
-__sfr __at (0xD0) PSW;
-__sfr __at (0xE0) ACC;
-__sfr __at (0xF0) B;
+__sfr __at(0x80) P0;
+__sfr __at(0x81) SP;
+__sfr __at(0x82) DPL;
+__sfr __at(0x83) DPH;
+__sfr __at(0x87) PCON;
+__sfr __at(0x88) TCON;
+__sfr __at(0x89) TMOD;
+__sfr __at(0x8A) TL0;
+__sfr __at(0x8B) TL1;
+__sfr __at(0x8C) TH0;
+__sfr __at(0x8D) TH1;
+__sfr __at(0x8E) AUXR;
+__sfr __at(0x8F) INTCLKO;  // External interrupt control
+__sfr __at(0x90) P1;
+__sfr __at(0x91) P1M1;
+__sfr __at(0x92) P1M0;
+__sfr __at(0x93) P0M1;
+__sfr __at(0x94) P0M0;
+__sfr __at(0x98) SCON;    // UART1 control register
+__sfr __at(0x99) SBUF;    // UART1 data buffer register
+__sfr __at(0x9D) IRCBAND; // IRC band selection register
+__sfr __at(0x9E) LIRTRIM; // IRC frequency trim register
+__sfr __at(0x9F) IRTRIM;  // IRC frequency adjustment register
+__sfr __at(0xA0) P2;
+__sfr __at(0xA2) P_SW1; // Peripheral port switch register 1
+__sfr __at(0xA8) IE;
+__sfr __at(0xA9) SADDR; // UART1 slave address register
+__sfr __at(0xB0) P3;
+__sfr __at(0xB1) P3M1;
+__sfr __at(0xB2) P3M0;
+__sfr __at(0xB8) IP;
+__sfr __at(0xB9) SADEN; // UART1 slave address enable register
+__sfr __at(0xBA) P_SW2; // Added for EAXFR access
+__sfr __at(0xC8) P5;
+__sfr __at(0xC9) P5M1;
+__sfr __at(0xCA) P5M0;
+__sfr __at(0xD0) PSW;
+__sfr __at(0xE0) ACC;
+__sfr __at(0xEF) AUXINTIF; // Auxiliary interrupt flags
+__sfr __at(0xF0) B;
 
 // Extended SFRs (XDATA area)
-__xdata __at (0xFE00) volatile uint8_t CKSEL;
-__xdata __at (0xFE01) volatile uint8_t CLKDIV;
-__xdata __at (0xFE02) volatile uint8_t HIRCCR;
-__xdata __at (0xFE03) volatile uint8_t XOSCCR;
-__xdata __at (0xFE04) volatile uint8_t IRC32KCR;
-__xdata __at (0xFE05) volatile uint8_t MCLKOCR;
-__xdata __at (0xFE13) volatile uint8_t P3PU;
-__xdata __at (0xFE15) volatile uint8_t P5PU;
-__xdata __at (0xFE33) volatile uint8_t P3IE;
-__xdata __at (0xFE35) volatile uint8_t P5IE;
+__xdata __at(0xFE00) volatile uint8_t CKSEL;
+__xdata __at(0xFE01) volatile uint8_t CLKDIV;
+__xdata __at(0xFE02) volatile uint8_t HIRCCR;
+__xdata __at(0xFE03) volatile uint8_t XOSCCR;
+__xdata __at(0xFE04) volatile uint8_t IRC32KCR;
+__xdata __at(0xFE05) volatile uint8_t MCLKOCR;
+__xdata __at(0xFE13) volatile uint8_t P3PU;
+__xdata __at(0xFE15) volatile uint8_t P5PU;
+__xdata __at(0xFE33) volatile uint8_t P3IE;
+__xdata __at(0xFE35) volatile uint8_t P5IE;
 
 // P_SW2 bits
 #define EAXFR 0x80
 
 // UART1 pin selection for STC8G1K08A
-#define S1_S_P30_P31  0x00  // RxD=P3.0, TxD=P3.1 (default)
-#define S1_S_P32_P33  0x40  // RxD=P3.2, TxD=P3.3
-#define S1_S_P54_P55  0x80  // RxD=P5.4, TxD=P5.5
+#define S1_S_P30_P31 0x00 // RxD=P3.0, TxD=P3.1 (default)
+#define S1_S_P32_P33 0x40 // RxD=P3.2, TxD=P3.3
+#define S1_S_P54_P55 0x80 // RxD=P5.4, TxD=P5.5
 
 #define MASK_TWO_BITS_HIGH 0x03
 
 // UART Mode definitions
-#define UART_MODE_1  0x01  // 8-bit UART, variable baud rate
+#define UART_MODE_1 0x01 // 8-bit UART, variable baud rate
 
 // Port 3 pins
-#define P3_0 16
-#define P3_1 17
-#define P3_2 18
-#define P3_3 19
+#define P3_0 0
+#define P3_1 1
+#define P3_2 2
+#define P3_3 3
 
 // Port 5 pins
-#define P5_4 28
-#define P5_5 29
+#define P5_4 4
+#define P5_5 5
+
+#define PORT3 3
+#define PORT5 5
+
+#define INT0_ISR_VECTOR 0
+#define INT1_ISR_VECTOR 2
+#define INT2_ISR_VECTOR 10
+#define INT3_ISR_VECTOR 11
+#define INT4_ISR_VECTOR 16
+#define TIMER0_ISR_VECTOR 1
+#define UART1_ISR_VECTOR 4
 
 #endif
